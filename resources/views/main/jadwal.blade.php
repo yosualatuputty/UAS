38622123da86920fa8d08539762c9acab51649f6
yosualatuputty <127925422+yosualatuputty@users.noreply.github.com>
2023-12-11 11:12:39 +0700

    Saved

--- resources/views/main/jadwal.blade.php
+++ resources/views/main/jadwal.blade.php
@@ -11,7 +11,7 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body data-bs-theme="dark">
+<body>
     @section('container')
         <div class="container mt-5" style="">
             <table class="table table-responsive table-sm table-bordered align-middle text-center">
